4c6b461a9cfb0406ee2f0df712d6c730d84a499e
Alexander Gunin <guninalexander@gmail.com>
2017-12-25 17:23:34 +0000

    change path

--- settings.py
+++ settings.py
@@ -9,11 +9,11 @@ MEAN_PIXEL_VALUE_NODULE = 41
 LUNA_SUBSET_START_INDEX = 0
 SEGMENTER_IMG_SIZE = 320
 
-BASE_DIR_SSD = "C:/werkdata/kaggle/ndsb3/"
-BASE_DIR = "D:/werkdata/kaggle/ndsb3/"
-EXTRA_DATA_DIR = "resources/"
-NDSB3_RAW_SRC_DIR = BASE_DIR + "ndsb_raw/stage12/"
-LUNA16_RAW_SRC_DIR = BASE_DIR + "luna_raw/"
+BASE_DIR_SSD = os.environ['TRAINING_DIR']
+BASE_DIR = os.environ['TRAINING_DIR']
+EXTRA_DATA_DIR = os.environ['BOWL_DIR'] + "/resources/"
+NDSB3_RAW_SRC_DIR = os.environ['BOWL_DIR'] + "/stage1/"
+LUNA16_RAW_SRC_DIR = os.environ['LUNA_DIR']
 
 NDSB3_EXTRACTED_IMAGE_DIR = BASE_DIR_SSD + "ndsb3_extracted_images/"
 LUNA16_EXTRACTED_IMAGE_DIR = BASE_DIR_SSD + "luna16_extracted_images/"
